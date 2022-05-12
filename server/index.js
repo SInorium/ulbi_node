cae6eed31d9392ef60f544ac34544ad11894edf7
OlegMikhailenko <oleg.mikhailenko@roko-labs.com>
2022-05-12 16:12:03 +0300

    configure-distribution-static
    реализовал возможность обращения к статике

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
@@ -14,9 +14,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(fileupload());
 app.use("/api", router);
-// app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileupload());
+app.use(express.static(path.resolve(__dirname, "static")));
 
 // Обработка ошибок, всегда полседний
 app.use(errorHandler);
