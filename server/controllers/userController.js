c43ba137681a4c99e2a1a12ccf150e6950749280
OlegMikhailenko <oleg.mikhailenko@roko-labs.com>
2022-05-17 13:59:46 +0300

    реализовал авторизацию

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,43 @@
 const ApiErros = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
 
+const generateJWT = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 class UserController {
-  async registration(req, res) {}
-  async login(req, res) {}
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiErros.badRequest("email or password is incorrect"));
+    }
+
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiErros.badRequest("user with this email already exists"));
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJWT(user.id, user.email, user.role);
+    res.json({ token });
+  }
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiErros.internal("user not found"));
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiErros.badRequest("user password mismatch"));
+    }
+    const token = generateJWT(user.id, user.email, user.role);
+    return res.json({ token });
+  }
   async check(req, res, next) {
     const { id } = req.query;
     if (!id) {
